9cfa9ba4541243b68b457736dec546a5ce88bc49
edwardpark <edwardpark97@gmail.com>
2016-06-13 22:25:00 -0700

    Convert a bunch of files at once.

--- generate_training_data.py
+++ generate_training_data.py
@@ -1,6 +1,7 @@
 import sys
 import wave
 import numpy as np
+import os.path
 
 from scipy.fftpack import dct
 from math import ceil
@@ -16,26 +17,30 @@ def generate_training_data(filename):
     if nchannels == 2 and width == 2:
         data = np.array([(ord(sample[i]) << 8) + ord(sample[i+1]) for i in range(0,len(sample),2)])
         data = data[0::2]
-    	wav.close()
+        wav.close()
         return data
     else:
         raise "Incompatible WAV File"
 
 def windowed_DFT(data):
-	num_windows = int(ceil(1. * data.size / WINDOW_SIZE))
-	dft_array = np.empty((num_windows, WINDOW_SIZE))
+    num_windows = int(ceil(1. * data.size / WINDOW_SIZE))
+    dft_array = np.empty((num_windows, WINDOW_SIZE))
 
-	for i in range(num_windows):
-		window = data[i * WINDOW_SIZE: (i+1) * WINDOW_SIZE]
-		if (window.size < WINDOW_SIZE):
-			window = np.copy(window)
-			window.resize(WINDOW_SIZE)
-		frequencies = dct(window, type=2, norm='ortho')
-		dft_array[i] = frequencies
+    for i in range(num_windows):
+        window = data[i * WINDOW_SIZE: (i+1) * WINDOW_SIZE]
+        if (window.size < WINDOW_SIZE):
+            window = np.copy(window)
+            window.resize(WINDOW_SIZE)
+        frequencies = dct(window, type=2, norm='ortho')
+        dft_array[i] = frequencies
 
-	return dft_array
+    return dft_array
 
 if __name__ == '__main__':
     output_dir = sys.argv[-1]
     for filename in sys.argv[1:-1]:
-        print windowed_DFT(generate_training_data(filename))
+        basename = os.path.splitext(os.path.basename(filename))[0]
+        print "working on file " + basename
+        output_file = basename + '.npz'
+        np.savez_compressed(os.path.join(output_dir, output_file), 
+                            windowed_DFT(generate_training_data(filename)))
