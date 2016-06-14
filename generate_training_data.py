c11dcafe6e73b1b25a09a910c664b58d62f17fcf
edwardpark <edwardpark97@gmail.com>
2016-06-13 20:59:11 -0700

    Numpy array to frequency array

--- generate_training_data.py
+++ generate_training_data.py
@@ -2,6 +2,12 @@ import sys
 import wave
 import numpy as np
 
+from scipy.fftpack import dct
+from math import ceil
+
+SAMPLING_RATE = 44100
+WINDOW_SIZE = SAMPLING_RATE / 20
+
 def generate_training_data(filename):
     wav = wave.open(filename, 'r')
     params = wav.getparams()
@@ -10,12 +16,23 @@ def generate_training_data(filename):
     if nchannels == 2 and width == 2:
         data = np.array([(ord(sample[i]) << 8) + ord(sample[i+1]) for i in range(0,len(sample),2)])
         data = data[0::2]
+    	wav.close()
+        return data
     else:
         raise "Incompatible WAV File"
-        
-    wav.close()
+
+def windowed_DFT(samples):
+	num_windows = int(ceil(1. * samples.size / WINDOW_SIZE))
+	dft_array = np.empty((num_windows, WINDOW_SIZE))
+
+	for i in range(num_windows):
+		window = samples[i * WINDOW_SIZE: (i+1) * WINDOW_SIZE]
+		frequencies = dct(window, type=2, norm='ortho')
+		dft_array[i] = frequencies
+
+	return dft_array
 
 if __name__ == '__main__':
     output_dir = sys.argv[-1]
     for filename in sys.argv[1:-1]:
-        generate_training_data(filename)
+        print windowed_DFT(generate_training_data(filename))
